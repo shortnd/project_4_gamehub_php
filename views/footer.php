194915b29873f08e7ce148a0cb62147d9ed5333f
Collin OConnell <ococncol@gmail.com>
2017-03-31 21:58:33 -0400

    added ajax and actions for forms

--- views/footer.php
+++ views/footer.php
@@ -6,9 +6,10 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <!-- Modal -->
     <div class="modal fade" id="logInModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -55,10 +56,18 @@
         }
       })
 
-      $("loginSignUpButton").click(function(){
-        alert("HI THERE!")
-      })
+      $('#loginSignUpButton').click(function(){
+        $.ajax({
+          type: "POST",
+          url: "actions.php?action=loginSignup",
+          data: "email=" + $("#email").val() + "&password=" + $("#password").val()
+          + "&$loginActive=" + $("#loginActive").val(),
+          success: function(res){
+            alert(res);
+          }
 
+        })
+      })
 
     </script>
   </body>
